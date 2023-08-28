8fd332a6bf4e3c84d70ae9c3f09d8703c654e0c9
Joe <hello@jlgn.io>
2023-08-28 15:21:20 +0800

    Initial

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>PlaySpark</title>
+        <meta
+          property="description"
+          content="Dynamic playable ads that generate a higher ROI and engagement for
+            your sports media website or app."
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
