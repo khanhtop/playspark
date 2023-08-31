10371195121ac75f983fa9520fa6f3e57e25e357
Joe <hello@jlgn.io>
2023-08-31 11:13:30 +0800

    Updates

--- pages/index.js
+++ pages/index.js
@@ -31,20 +31,30 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex flex-col justify-center">
-        <div className="bg-gradient-to-b from-white to-indigo-500  h-screen w-screen  flex w-full max-w-[1200px] px-4 md:px-12 gap-2">
+      <div className="w-screen flex flex-col justify-center bg-gradient-to-b from-white to-indigo-500  items-center ">
+        <div className="gap-8 h-screen w-screen  flex w-full max-w-[1200px] px-4 md:px-12 gap-2">
           <div className="flex-1 flex flex-col justify-center items-center md:items-start">
             <img className="-my-[10%] -mx-[8%] " src="/branding/logo.png" />
-            <h3 className="text-2xl md:text-6xl text-white font-bold">
+            <h3 className="text-2xl md:text-3xl lg:text-6xl text-white font-bold">
               SPORTS ADVERTISING
             </h3>
-            <h3 className="text-3xl md:text-6xl text-white font-bold mb-8 ">
+            <h3 className="text-3xl md:text-3xl lg:text-6xl text-white font-bold md:mb-8 mb-4 ">
               SUPERCHARGED
             </h3>
             <h5 className="md:text-xl text-white/60 font-extralight text-center md:text-left">
               Dynamic playable ads that generate a higher ROI and engagement for
               your sports media website or app.
             </h5>
+            <MuxPlayer
+              playbackId="jes2JpY021gTDjtGdbxMMUq6BjSgcdJXjdMJ7CcFmb2Q"
+              className="mt-4 block md:hidden"
+              metadata={{
+                video_id: "video-id-123456",
+                video_title: "Bick Buck Bunny",
+                viewer_user_id: "user-id-bc-789",
+              }}
+              streamType="on-demand"
+            />
             {reveal ? (
               <div className="flex h-10 mt-8">
                 <input
@@ -75,32 +85,18 @@ export default function Home() {
               </button>
             )}
           </div>
-          <div className="hidden w-[400px] md:flex items-center">
-            <img src="/branding/header-image.png" />
+          <div className="hidden flex-1 md:flex items-center">
+            <MuxPlayer
+              playbackId="jes2JpY021gTDjtGdbxMMUq6BjSgcdJXjdMJ7CcFmb2Q"
+              className="mt-4"
+              metadata={{
+                video_id: "video-id-123456",
+                video_title: "Bick Buck Bunny",
+                viewer_user_id: "user-id-bc-789",
+              }}
+              streamType="on-demand"
+            />
           </div>
-        </div>
-        <div className="bg-indigo-500 flex flex-col items-center">
-          <div className="flex flex-col items-center">
-            <h1 className="text-3xl uppercase text-white">Learn More</h1>
-            <div className="flex gap-2">
-              <p className="text-white/75">Watch our introductory video</p>
-              <ChevronDownIcon className="h-6 w-6 animate-bounce text-white/75" />
-            </div>
-          </div>
-
-          <MuxPlayer
-            playbackId="jes2JpY021gTDjtGdbxMMUq6BjSgcdJXjdMJ7CcFmb2Q"
-            className="mt-4"
-            metadata={{
-              video_id: "video-id-123456",
-              video_title: "Bick Buck Bunny",
-              viewer_user_id: "user-id-bc-789",
-            }}
-            streamType="on-demand"
-          />
-        </div>
-        <div className="text-white py-12 bg-indigo-500 flex flex-col items-center">
-          <p>(c) PlaySpark 2023</p>
         </div>
       </div>
     </>
