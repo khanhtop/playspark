0b09e00ecf01f70b04c3cb4921013ab04a298e2c
kooldart <kadriudrin@gmail.com>
2023-09-12 19:10:08 +0200

    Added easy way to change game assets

--- components/games/pong.js
+++ components/games/pong.js
@@ -1,35 +1,62 @@
-import dynamic from 'next/dynamic';
-import { useRef } from 'react';
+import dynamic from "next/dynamic";
+import { useRef } from "react";
 
-const MiddlePong = dynamic(() => import('./pongGame'), {
-    ssr: false,
+const MiddlePong = dynamic(() => import("./pongGame"), {
+  ssr: false,
 });
 
 export default function Pong({ data }) {
-    const childRef = useRef();
+  const childRef = useRef();
 
-    const startNewGame = () => {
-        // this is how to start a new game with 5 lives
-        childRef.current.initGame(5);
-    }
+  const startNewGame = () => {
+    // this is how to start a new game with 5 lives
+    childRef.current.initGame(5);
+  };
 
-    const scoreHandler = (score) => {
-        // this is where you can handle the score event when game is finished
-        alert("YOU WON: " + score)
-    }
+  const scoreHandler = (score) => {
+    // this is where you can handle the score event when game is finished
+    alert("YOU WON: " + score);
+  };
 
-    return (
-        <div
-            style={{ backgroundColor: data?.primaryColor, color: data?.textColor, width: '500px', height: '884px' }}
-            className={`aspect-w-9 aspect-h-19 h-full w-auto relative flex flex-col`}
-        >
-            <div style={{ fontFamily: 'enhanced_led_board-7', visibility: 'hidden', height: '0px', width: '0px' }}>.</div>
-            <div style={{ fontFamily: 'TitanOne-Regular', visibility: 'hidden', height: '0px', width: '0px' }}>.</div>
-            <div className="w-full h-24 bg-white/20 flex items-center justify-center">
-                <p>Branding Banner Goes Here</p>
-                <button onClick={startNewGame}>Restart with reward</button>
-            </div>
-            <MiddlePong pongRef={childRef} handleScore={scoreHandler} />
-        </div>
-    );
+  return (
+    <div
+      style={{
+        backgroundColor: data?.primaryColor,
+        color: data?.textColor,
+        width: "500px",
+        height: "884px",
+      }}
+      className={`aspect-w-9 aspect-h-19 h-full w-auto relative flex flex-col`}
+    >
+      <div
+        style={{
+          fontFamily: "enhanced_led_board-7",
+          visibility: "hidden",
+          height: "0px",
+          width: "0px",
+        }}
+      >
+        .
+      </div>
+      <div
+        style={{
+          fontFamily: "TitanOne-Regular",
+          visibility: "hidden",
+          height: "0px",
+          width: "0px",
+        }}
+      >
+        .
+      </div>
+      <div className="w-full h-24 bg-white/20 flex items-center justify-center">
+        <p>Branding Banner Goes Here</p>
+        <button onClick={startNewGame}>Restart with reward</button>
+      </div>
+      <MiddlePong
+        pongRef={childRef}
+        handleScore={scoreHandler}
+        gameType={"football"}
+      />
+    </div>
+  );
 }
