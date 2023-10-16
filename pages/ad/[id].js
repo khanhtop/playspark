3fdb9709a509604f9810710aa3751a8160c885e8
Joe Logan <hello@jlgn.io>
2023-10-17 08:18:29 +0900

    Updated

--- pages/ad/[id].js
+++ pages/ad/[id].js
@@ -1,12 +1,51 @@
 import Advert from "@/components/ad";
 import { getAd } from "@/helpers/api";
+import Head from "next/head";
 import { useEffect, useState } from "react";
 
 export default function Ad({ ad, id }) {
+  console.log(ad);
+
+  const getImageURL = (url) => {
+    if (url.startsWith("http")) return url;
+    return "https://playspark.co" + url;
+  };
   return (
-    <div className="text-white font-bold ">
-      {ad ? <Advert data={ad} /> : <p>{id} - AD NOT FOUND</p>}
-    </div>
+    <>
+      <Head>
+        <title>Play {ad.name} at PlaySpark</title>
+        <meta
+          name="description"
+          content={`Play ${ad.name} and many other awesome games at PlaySpark.co`}
+        />
+        <meta
+          property="og:url"
+          content={`https://playspark.co/ad/${ad.tournamentId.toString()}`}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={`Play ${ad.name} at PlaySpark`} />
+        <meta
+          property="og:description"
+          content={`Play ${ad.name} and many other awesome games at PlaySpark.co`}
+        />
+        <meta property="og:image" content={getImageURL(ad.backgroundImage)} />
+        <meta name="twitter:card" content={getImageURL(ad.backgroundImage)} />
+        <meta property="twitter:domain" content="playspark.co" />
+        <meta
+          property="twitter:url"
+          content="https://playspark.co/ad/1696389045606"
+        />
+        <meta name="twitter:title" content={`Play ${ad.name} at PlaySpark`} />
+        <meta
+          name="twitter:description"
+          content={`Play ${ad.name} and many other awesome games at PlaySpark.co`}
+        />
+        <meta name="twitter:image" content={getImageURL(ad.backgroundImage)} />
+      </Head>
+      <div className="text-white font-bold ">
+        {ad ? <Advert data={ad} /> : <p>{id} - AD NOT FOUND</p>}
+      </div>
+    </>
   );
 }
 
