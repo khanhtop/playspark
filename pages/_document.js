00caae075912a83172139c83191b3b01861c15f9
Joe <hello@jlgn.io>
2023-10-27 09:43:53 +0800

    Upload

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,18 @@ export default function Document() {
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1479162116573574"
           crossorigin="anonymous"
         />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-51CE0JP1N8`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-51CE0JP1N8');`,
+          }}
+        />
       </Head>
       <body>
         <Main />
