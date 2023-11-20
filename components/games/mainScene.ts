b95358a7976e912f9dca83ead2176b4412c23b29
Joe Logan <hello@jlgn.io>
2023-11-20 15:07:35 +0900

    Updated

--- components/games/mainScene.ts
+++ components/games/mainScene.ts
@@ -105,7 +105,7 @@ export default class MainScene extends Phaser.Scene {
       false
     );
     this.add.image(0, 0, "bg").setOrigin(0).setDisplaySize(w, h);
-    this.add.image(mW, mH, "middleAd").setDisplaySize(50, 50).setAlpha(1);
+    this.add.image(mW, mH, "middleAd").setDisplaySize(50, 50).setAlpha(0);
     //this.add.image(0, 0, 'bg').setOrigin(0).setDisplaySize(w, h);
 
     this.add.image(mW, 37, "score").setDisplaySize(scrW, scrH);
