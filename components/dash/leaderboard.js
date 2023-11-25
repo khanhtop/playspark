8e6c8e10c289aaa3ba2eb377af7b77d31da10e46
Joe Logan <hello@jlgn.io>
2023-11-26 05:59:27 +0700

    Updates

--- components/dash/leaderboard.js
+++ components/dash/leaderboard.js
@@ -6,8 +6,10 @@ export default function Leaderboard({
   primaryColor,
   textColor,
 }) {
+  console.log(data);
   return (
     <div className="py-4 h-full w-full overflow-y-scroll text-black top-0 left-0 w-full h-full flex flex-col items-center gap-2">
+      <RankTriangles data={data?.slice(0, 3)} />
       {data.map((item, key) => (
         <div key={key} className="flex w-full px-8 gap-4 items-center">
           <div
@@ -24,6 +26,30 @@ export default function Leaderboard({
           </Text>
         </div>
       ))}
+    </div>
+  );
+}
+
+function RankTriangles({ data }) {
+  return (
+    <div className="h-24 w-full flex mb-4 gap-1">
+      <Triangle data={data?.[1]} position={2} className="w-24 h-[80%]" />
+      <Triangle data={data?.[0]} position={1} className="flex-1 h-full" />
+      <Triangle data={data?.[2]} position={3} className="w-24 h-[80%]" />
+    </div>
+  );
+}
+
+function Triangle({ className, position, data }) {
+  return (
+    <div
+      className={`relative ${className} font-octo text-lg bg-[#DDD] rounded-lg flex flex-col items-center justify-center`}
+    >
+      <div className="flex items-center justify-center font-octo text-white absolute -bottom-4 h-8 w-8 bg-cyan-500 rounded-full">
+        {position}
+      </div>
+      <p>{data?.name ?? "?"}</p>
+      <p>{data?.score ?? "-"}</p>
     </div>
   );
 }
