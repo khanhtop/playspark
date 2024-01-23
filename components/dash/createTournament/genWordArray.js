571ce1d5b2fea5f48791df3d299b9290551db880
Joe Logan <hello@jlgn.io>
2024-01-23 15:18:08 +0800

    Updates

--- components/dash/createTournament/genWordArray.js
+++ components/dash/createTournament/genWordArray.js
@@ -1,7 +1,9 @@
 import Button from "@/components/forms/button";
 import Input from "@/components/forms/input";
 import { getWordlePrompt, wordleSystemPrompt } from "@/helpers/prompts";
-import { TrashIcon } from "@heroicons/react/24/solid";
+import { getRandomWordsByTheme, pregenWordles } from "@/helpers/wordGames";
+import { custom } from "@cloudinary/url-gen/qualifiers/region";
+import { PencilIcon, TrashIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 
 export default function GenWordArray({
@@ -11,17 +13,25 @@ export default function GenWordArray({
   setWords,
   setTheme,
 }) {
+  const [gameTheme, setGameTheme] = useState("other");
+  const [predefinedWords, setPredefinedWords] = useState(
+    getRandomWordsByTheme("other")
+  );
   const [currentWord, setCurrentWord] = useState("");
   const [generating, setGenerating] = useState(false);
+  const [customWords, setCustomWords] = useState([]);
 
   const addWord = () => {
     setWords([...tournament.words, currentWord.toUpperCase()]);
+    setCustomWords([...customWords, currentWord.toUpperCase()]);
     setCurrentWord("");
   };
 
+  console.log(customWords, tournament.words);
+
   const generateWords = async () => {
     const messages = [
-      getWordlePrompt(tournament.wordleTheme || "Sports"),
+      getWordlePrompt(),
       { role: "user", content: tournament.words.join(",") },
     ];
     const response = await fetch("/api/internal/gpt", {
@@ -41,69 +51,143 @@ export default function GenWordArray({
     }
   };
 
+  const customWordCount = customWords.length;
+  const predefinedWordCount = tournament.words.length - customWords.length;
+
   return (
     <div className="flex flex-col">
-      <Input
-        label="Theme (A single word to describe the theme of the game, e.g. Baseball"
-        className="bg-white/5 flex-1 w-full py-2 mb-4 text-white"
-        placeHolder="Theme"
-        value={tournament.wordleTheme}
-        labelColor="text-white/70"
-        onChange={(e) => setTheme(e.target.value)}
-      />
-      <div className="flex w-full gap-4">
-        <Input
-          label={title}
-          className="bg-white/5 flex-1 w-full py-2 mb-4 text-white"
-          placeHolder="Type a word"
-          maxLength={maxLength}
-          value={currentWord}
-          labelColor="text-white/70"
-          onChange={(e) => setCurrentWord(e.target.value)}
-        />
-        <Button
-          onClick={() => addWord()}
-          disabled={currentWord.length < 5}
-          className="mt-5 px-8"
-        >
-          Add
-        </Button>
-      </div>
-      <div className="flex flex-wrap gap-4">
-        {tournament.words.map((item, key) => (
-          <Word
-            word={item}
-            key={key}
-            onClick={() => {
-              setWords(tournament.words.filter((a) => a !== item));
-            }}
-          />
-        ))}
-        {tournament.words.length > 2 && (
-          <Button
-            loading={generating}
-            onClick={async () => {
-              const res = await generateWords();
-              setWords([...tournament.words, ...res]);
-            }}
-            className="w-36 bg-transparent border-cyan-500 border-2 text-white"
-          >
-            Generate More
-          </Button>
-        )}
-      </div>
+      <p className="text-xs text-white mb-1">Game Theme</p>
+      <select
+        value={gameTheme}
+        className="bg-transparent text-white w-full h-10 border-cyan-400/50 border-[1px]"
+        onChange={(e) => {
+          setGameTheme(e.target.value);
+          setPredefinedWords(getRandomWordsByTheme(e.target.value));
+          setWords([]);
+          setCustomWords([]);
+        }}
+      >
+        <option value="other">Other</option>
+        {Object.keys(pregenWordles).map((item, key) => {
+          return <option value={item}>{item}</option>;
+        })}
+      </select>
+
+      <>
+        <p className="text-xs text-white mb-1 mt-4">
+          Select Predefined Words {`(${predefinedWordCount} Added)`}
+        </p>
+        <div className="text-white flex flex-wrap gap-2">
+          {predefinedWords.map((item, key) => {
+            return (
+              <Word
+                added={tournament.words.includes(item)}
+                word={item}
+                predefined={true}
+                onAdd={(w) => {
+                  setWords([...tournament.words, w]);
+                }}
+                onRemove={(w) => {
+                  setWords(tournament.words.filter((a) => a !== w));
+                }}
+              />
+            );
+          })}
+        </div>
+        <div className="flex-col flex">
+          <div className="flex w-full gap-4 mt-4">
+            <Input
+              label={`Add Your Own Words (${customWordCount} Added)`}
+              className="bg-white/5 flex-1 w-full py-2 mb-4 text-white"
+              placeHolder="Enter A Word"
+              maxLength={maxLength}
+              value={currentWord}
+              labelColor="text-white/70"
+              onChange={(e) => setCurrentWord(e.target.value)}
+            />
+            <Button
+              onClick={() => addWord()}
+              disabled={currentWord.length < 5}
+              className="mt-5 px-8"
+            >
+              Add
+            </Button>
+          </div>
+          <div className="flex flex-wrap gap-4">
+            {customWords.map((item, key) => (
+              <Word
+                word={item}
+                key={key}
+                onClick={() => {
+                  setWords(tournament.words.filter((a) => a !== item));
+                  setCustomWords(customWords.filter((a) => a !== item));
+                }}
+              />
+            ))}
+            {customWords.length > 0 && tournament.words.length > 2 && (
+              <Button
+                loading={generating}
+                onClick={async () => {
+                  const res = await generateWords();
+                  setWords([...tournament.words, ...res]);
+                  setCustomWords([...customWords, ...res]);
+                }}
+                className="w-36 bg-transparent border-cyan-500 border-2 text-white"
+              >
+                Generate More
+              </Button>
+            )}
+          </div>
+        </div>
+      </>
     </div>
   );
 }
 
-function Word({ word, onClick }) {
+function EditableWord({ word, added, predefined, onClick, onAdd, onRemove }) {
   return (
     <div
-      onClick={onClick}
-      className="flex cursor-warning transition group items-center justify-center gap-2 text-black bg-cyan-500 hover:bg-red-500 w-36 py-2 rounded-lg"
+      onClick={() => {
+        if (predefined && added) {
+          onRemove(word);
+        } else if (predefined && !added) {
+          onAdd(word);
+        }
+      }}
+      className={`cursor-pointer flex cursor-warning transition group items-center justify-center gap-2 text-black  ${
+        added || !predefined
+          ? "bg-green-500"
+          : "bg-cyan-500/50 hover:bg-green-500/70"
+      } w-36 py-2 rounded-lg`}
     >
       <p>{word}</p>
-      <TrashIcon className="hidden group-hover:flex h-5 w-5" />
+      {!predefined && (
+        <PencilIcon className="hidden group-hover:flex h-5 w-5" />
+      )}
+    </div>
+  );
+}
+
+function Word({ word, added, predefined, onClick, onAdd, onRemove }) {
+  return (
+    <div
+      onClick={() => {
+        if (predefined && added) {
+          onRemove(word);
+        } else if (predefined && !added) {
+          onAdd(word);
+        } else {
+          onClick();
+        }
+      }}
+      className={`cursor-pointer flex cursor-warning transition group items-center justify-center gap-2 text-black  ${
+        added || !predefined
+          ? "bg-green-500"
+          : "bg-cyan-500/50 hover:bg-green-500/70"
+      } w-36 py-2 rounded-lg`}
+    >
+      <p>{word}</p>
+      {!predefined && <TrashIcon className="hidden group-hover:flex h-5 w-5" />}
     </div>
   );
 }
--- helpers/prompts.js
+++ helpers/prompts.js
@@ -3,9 +3,9 @@ export const wordleSystemPrompt = {
   content: `Your task is to generate a JSON array of 5-letter words for use in a Wordle game.  The user will provide five examples, and you must add another five.  You must respond only in JSON`,
 };
 
-export const getWordlePrompt = (context) => {
+export const getWordlePrompt = () => {
   return {
     role: "system",
-    content: `Your task is to generate a JSON array of 5-letter words for use in a Wordle game, based upon the theme ${context}.  The user will provide a list of words that they already have, and your task is to add five more words. You must not respond with any repeat words that the user has already provided. You must provide only five letter words. You must only respond in JSON as follows: {"words": []}`,
+    content: `Your task is to generate a JSON array of 5-letter words for use in a Wordle game, based upon the theme of the words provided by the user.  The user will provide a list of words that they already have, and your task is to add five more words. You must not respond with any repeat words that the user has already provided. You must provide only five letter words. You must only respond in JSON as follows: {"words": []}`,
   };
 };
