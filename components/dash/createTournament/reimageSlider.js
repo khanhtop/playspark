8da8f3ef9a371d0368db1a28c41e250c16525bcd
Joe <hello@jlgn.io>
2024-04-19 07:41:48 +0800

    Updated

--- components/dash/createTournament/reimageSlider.js
+++ components/dash/createTournament/reimageSlider.js
@@ -3,21 +3,21 @@ import { useState, useEffect } from "react";
 export default function ReimageGrid({}) {
   //   const [stateImages, setStateImages] = useState();
 
-  //   useEffect(() => {
-  //     if (!stateImages) {
-  //       fetch(`https://api.reimage.dev/get/tags?avatar`, {
-  //         headers: {
-  //           Authorization: `Bearer ${process.env.NEXT_PUBLIC_REIMAGE_KEY}`,
-  //         },
+  // useEffect(() => {
+  //   if (!stateImages) {
+  //     fetch(`https://api.reimage.dev/get/tags?avatar`, {
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.NEXT_PUBLIC_REIMAGE_KEY}`,
+  //       },
+  //     })
+  //       .then((raw) => {
+  //         return raw.json();
   //       })
-  //         .then((raw) => {
-  //           return raw.json();
-  //         })
-  //         .then((json) => {
-  //           setStateImages(json);
-  //         });
-  //     }
-  //   }, []);
+  //       .then((json) => {
+  //         setStateImages(json);
+  //       });
+  //   }
+  // }, []);
 
   //   console.log(stateImages);
 
