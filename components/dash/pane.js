90267136280a6fdf0d0b6a4837a01008fb69f908
Joe <hello@jlgn.io>
2024-07-16 08:21:31 +0800

    Updated

--- components/dash/pane.js
+++ components/dash/pane.js
@@ -1,10 +1,13 @@
-export default function Pane({ title, children }) {
+export default function Pane({ title, subtext, children }) {
   return (
-    <div className="flex-1 pt-12 px-8 overflow-y-scroll">
-      <h1 className="mb-8 font-bold text-black/60 text-2xl tracking-wide">
-        {title}
-      </h1>
-      {children}
+    <div className="flex-1 pt-12 px-8 flex flex-col">
+      <div className="pb-4 border-b-white/100 border-b-2 mb-4">
+        <h1 className="font-bold text-blue-950 text-2xl tracking-wide">
+          {title}
+        </h1>
+        <p className="text-black/30">{subtext}</p>
+      </div>
+      <div className="flex-1 overflow-y-scroll">{children}</div>
     </div>
   );
 }
