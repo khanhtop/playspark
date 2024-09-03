d5975ae169cb234bfc0bde8e5057bc3ca6bb0d5f
Joe <hello@jlgn.io>
2024-09-03 10:24:50 +0800

    Updated

--- components/dash/mygames.js
+++ components/dash/mygames.js
@@ -11,6 +11,7 @@ import FilterPills from "./filterPills";
 import Button from "../forms/button";
 import { FolderIcon } from "@heroicons/react/24/solid";
 import CreateModal from "./createTournament/createModal";
+import Swal from "sweetalert2";
 
 export default function MyGames({}) {
   const context = useAppContext();
@@ -115,8 +116,31 @@ export default function MyGames({}) {
                 },
                 {
                   text: item.isActive ? "End Tournament" : "Restart Tournament",
-                  action: () =>
-                    switchActive(item.tournamentId, item?.isActive ?? false),
+                  action: () => {
+                    Swal.fire({
+                      icon: "question",
+                      title: item.isActive
+                        ? "End Tournament"
+                        : "Restart Tournament",
+                      text: item.isActive
+                        ? "Are you sure you want to end the tournament? The tournament will no longer be accessible, and all end-game rewards will be distributed."
+                        : "Restart Tournament",
+                      showCancelButton: true,
+                      confirmButtonText: item.isActive
+                        ? "End Tournament"
+                        : "Restart Tournament",
+                      confirmButtonColor: "#3085d6",
+                      cancelButtonColor: "#AAA",
+                    }).then((result) => {
+                      /* Read more about isConfirmed, isDenied below */
+                      if (result.isConfirmed) {
+                        Swal.fire("Saved!", "", "success");
+                      } else if (result.isDenied) {
+                        Swal.fire("Changes are not saved", "", "info");
+                      }
+                    });
+                    // switchActive(item.tournamentId, item?.isActive ?? false),
+                  },
                 },
                 {
                   text: "View Analytics",
