0a59762552dac3b84e46ed3366cab25d993db54c
Joe Logan <hello@jlgn.io>
2024-09-24 06:38:37 +0900

    Updated

--- helpers/reimage.js
+++ helpers/reimage.js
@@ -2,3 +2,14 @@ export const migrate = (url) => {
   console.log(url);
   return url;
 };
+
+export const cloudinaryToReimage = (url, dimensions, format = "webp") => {
+  if (!url) return url;
+  if (!url.includes("res.cloudinary.com")) return url;
+  const fileName = url.split("/").pop();
+  const objectId = fileName.split(".")[0];
+  let rendition = "original";
+  if (dimensions) rendition = `${dimensions}.${format}`;
+  const reimageUrl = `https://files.reimage.dev/playspark/${objectId}/${rendition}`;
+  return reimageUrl;
+};
