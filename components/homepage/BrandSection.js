80f15cdbac8633f88662f574a3f3ad1a22516a75
khanhtop <khanhtopdev@gmail.com>
2024-10-02 22:35:35 +0800

    fixed pixel issue

--- components/homepage/BrandSection.js
+++ components/homepage/BrandSection.js
@@ -12,10 +12,10 @@ export default function BrandSection({ page }) {
         <img src={Ticker.src} />
         <button className="bg-button_level w-[241px] text-black rounded-lg py-3 px-3 ">See Case Studies</button>
       </div>
-      <div className="bg-gradient-to-t from-back to-white py-16 px-4 flex flex-col items-center justify-center  gap-5 text-black bg-white">
-          <div className="lg:shadow-sm lg:shadow-grey lg:border  lg:rounded-[10px] px-[105px]  pb-4">
+      <div className="bg-gradient-to-t from-back to-white py-16 px-4 flex flex-col items-center justify-center gap-5 text-black bg-white">
+          <div className="lg:shadow-sm lg:shadow-grey lg:border  lg:rounded-[10px] lg:px-[105px] pb-4">
             <h1 className="my-4 font-bold text-[54px]">{page.brand_title_2}</h1>
-            <p className="text-[#6F6C90] text-[22px] w-[535px] mx-auto">
+            <p className="text-[#6F6C90] text-[22px] lg:w-[535px] mx-auto">
               {" "}
               {page.brand_text}
             </p>
@@ -39,16 +39,13 @@ const Blog = ({ item }) => {
 
   const imagePositionClassName = React.useMemo(() => {
     if(item.image_position == null){
-      console.log("1");
       className = "absolute right-[-50px]"
     }
     else if(item.image_position == "top-right") {
-      console.log("2")
       className = "absolute top-0 right-0 "
     }
 
     else if(item.image_position == "bottom-right"){
-      console.log("3")
       className = "absolute bottom-0 right-[-30px] "
     }
   }, [item.image_position])
