9bb789b5b0d9d2a47d4336fc52d074986a5020b7
khanhtop <khanhtopdev@gmail.com>
2024-10-22 04:02:46 +0800

    change icon

--- components/study/Items.js
+++ components/study/Items.js
@@ -1,5 +1,5 @@
 import { PrismicRichText } from "@prismicio/react";
-import selectImage from "../../public/images/select.png"
+import selectImage from "../../public/images/select.png";
 
 export default function Items({ page }) {
   return (
@@ -13,36 +13,44 @@ export default function Items({ page }) {
   );
 }
 
-const Blog = ({item}) => {
+const Blog = ({ item }) => {
   return (
-    <div className="mx-auto max-w-[1000px] " >
+    <div className="mx-auto max-w-[1000px] ">
       <p className="text-[54px] text-center font-bold ">{item.title}</p>
       <div className=" flex flex-col-reverse lg:flex-row gap-5  shadow-xl shadow-grey border rounded-[10px] my-5 py-10">
         <div className="lg:w-1/2  w-full flex flex-col gap-10 items-center justify-center">
-          <h1 className=" font-bold text-2xl px-12 text-start">{item.blog_title}</h1>
-          <div className=" flex flex-col gap-8 text-start text-[16px] px-3">
-            <div className=" flex flex-row gap-1 justify-center items-center"> 
-                {item.blog_text_1 && <SelectImg />}
-                <p className="text-[22px] text-start">{item.blog_text_1}</p>
+          <h1 className=" font-bold text-2xl px-12 text-start">
+            {item.blog_title}
+          </h1>
+          <div className=" flex flex-col gap-8 text-start text-[16px] px-3 items-start">
+            <div className=" flex flex-row gap-1 justify-center items-center">
+              {item.blog_text_1 && <SelectImg item={item} num="1" />}
+              <p className="text-[22px] text-start">{item.blog_text_1}</p>
             </div>
-            <div className=" flex flex-row gap-1 justify-center items-center"> 
-            {item.blog_text_2 && <SelectImg />}
-            <p className="text-[22px] text-start">{item.blog_text_2}</p>
+            <div className=" flex flex-row gap-1 justify-center items-center">
+              {item.blog_text_2 && <SelectImg item={item} num="2" />}
+              <p className="text-[22px] text-start">{item.blog_text_2}</p>
+            </div>
+            <div className=" flex flex-row gap-1 justify-center items-center">
+              {item.blog_text_3 && <SelectImg item={item} num="3" />}
+              <p className="text-[22px] text-start">{item.blog_text_3}</p>
             </div>
           </div>
-      </div>
-      <div className="w-full lg:w-1/2 flex items-center justify-center">
-          <img src = {item.blog_image.url}  />
-      </div>
+        </div>
+        <div className="w-full lg:w-1/2 flex items-center justify-center">
+          <img src={item.blog_image.url} />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-const SelectImg = () => {
-    return(
-      <img src={selectImage.src}/>
-    ) 
+const SelectImg = ({ num, item }) => {
+  if (num == "1" && item.icon_1) {
+    return <img src={item.icon_1.url} />;
+  } else if (num == "2" && item.icon_2) {
+    return <img src={item.icon_2.url} />;
+  } else if (num == "3" && item.icon_3) {
+    return <img src={item.icon_3.url} />;
   }
-
+};
