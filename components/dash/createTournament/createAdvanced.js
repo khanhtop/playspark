2fc93de1820b15cc629c7dddc55c117e04b18ca3
Joe Logan <hello@jlgn.io>
2024-11-10 06:53:33 +0900

    Updated

--- components/dash/createTournament/createAdvanced.js
+++ components/dash/createTournament/createAdvanced.js
@@ -1,10 +1,32 @@
+import { Card, RangeSlider } from "flowbite-react";
 import { BrandingComponent } from "../unlocksWithTier";
 import Toggle from "react-toggle";
+import { useMemo } from "react";
 
 export default function CreateAdvanced({ tournament, setTournament }) {
+  const difficulty = useMemo(() => {
+    return (tournament.winProbability * 100).toFixed(0) + "/100";
+  }, [tournament.winProbability]);
+
   return (
-    <>
-      <BrandingComponent>
+    <div className="flex flex-col gap-4">
+      <Card className="flex-1 w-full">
+        <h3 className="text-black/50">Game Difficulty {difficulty}</h3>
+        <div className="w-full">
+          <RangeSlider
+            min={0}
+            max={1}
+            step={0.01}
+            value={tournament.winProbability}
+            className="w-full"
+            onChange={(e) =>
+              setTournament({ ...tournament, winProbability: e.target.value })
+            }
+          />
+        </div>
+      </Card>
+      <Card>
+        <h3 className="text-black/50">Game Tweaks</h3>
         <div className="flex flex-col">
           <div className="flex items-center gap-2 mb-4">
             <p className="text-black/70">Hide Back Navigation</p>
@@ -19,8 +41,6 @@ export default function CreateAdvanced({ tournament, setTournament }) {
             />
           </div>
         </div>
-      </BrandingComponent>
-      <BrandingComponent>
         <div className="flex flex-col">
           <div className="flex items-center gap-2 mb-4">
             <p className="text-black/70">Disable Revive Function</p>
@@ -35,7 +55,7 @@ export default function CreateAdvanced({ tournament, setTournament }) {
             />
           </div>
         </div>
-      </BrandingComponent>
-    </>
+      </Card>
+    </div>
   );
 }
